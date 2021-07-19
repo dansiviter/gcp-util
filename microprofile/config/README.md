fbab6d2f532eefcb46754efc221cb83b7ed723a2
Daniel Siviter <dansiviter@gmail.com>
2021-07-19 21:14:00 +0100

    Adding metadata source

--- microprofile/config/README.md
+++ microprofile/config/README.md
@@ -1,8 +1,20 @@
 # Microprofile Config GCP Integration
 
+## MetaData ##
+
+A few values values are able to be extracted from the MetaData Server:
+* `gcp.project-id` - Default project identifier,
+* `gcp.cluster-name` - Cluster name,
+* `gcp.cluster-location` - Cluster location,
+* `gcp.default-sa.email` - Default service account email.
+
+These are useful when using property expressions to avoid repetition. For example, for Cloud SQL instances are in the same project: `${gcp.project-id}:REGION:INSTANCE-ID`
+
 ## Secret Manager ##
 
-Supports 3 formats of secret naming:
+By default, this is not enabled. To enable leverage the Service Loader mechanism via `META-INF/services/org.eclipse.microprofile.config.spi.ConfigSource`.
+
+Three formats of secret naming are supported:
 * `secrets/{secret}` - This will default to current `projectId` and `latest` version,
 * `secrets/{secret}/versions/{version}` - This will default to current `projectId`,
 * `projects/{project}/secrets/{secret}/versions/{version}` - Standard GCP naming convention.
---
+++ microprofile/config/src/main/java/uk/dansiviter/gcp/microprofile/config/MetadataConfigSource.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019-2021 Daniel Siviter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.dansiviter.gcp.microprofile.config;
+
+import static java.util.Collections.emptyMap;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.google.cloud.MetadataConfig;
+import com.google.cloud.ServiceOptions;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+class MetadataConfigSource implements ConfigSource {
+	private static final Map<String, Supplier<String>> MAP = Map.of(
+		"gcp.project-id", ServiceOptions::getDefaultProjectId,
+		"gcp.cluster-name", MetadataConfig::getClusterName,
+		"gcp.cluster-location", () -> MetadataConfig.getAttribute("instance/attributes/cluster-location"),
+		"gcp.default-sa.email", () -> MetadataConfig.getAttribute("instance/service-accounts/default/email")
+	);
+
+	@Override
+	public Map<String, String> getProperties() {
+		return emptyMap();
+	}
+
+	@Override
+	public String getValue(String propertyName) {
+		var supplier = MAP.get(propertyName);
+		return supplier != null ? supplier.get() : null;
+	}
+
+	@Override
+	public String getName() {
+		return "gcp-metadata-server";
+	}
+}
