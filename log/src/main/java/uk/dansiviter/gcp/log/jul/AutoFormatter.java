7eeb265e22a9a5f0619c10bdfad32d7185808c6b
Daniel Siviter <dansiviter@gmail.com>
2022-10-27 19:36:26 +0100

    <init> speed improvement

--- log/src/main/java/uk/dansiviter/gcp/log/jul/AutoFormatter.java
+++ log/src/main/java/uk/dansiviter/gcp/log/jul/AutoFormatter.java
@@ -1,22 +1,18 @@
 package uk.dansiviter.gcp.log.jul;
 
-import static java.lang.Thread.currentThread;
-import static java.net.http.HttpResponse.BodyHandlers.discarding;
 import static java.util.logging.LogManager.getLogManager;
 import static uk.dansiviter.jule.JulUtil.property;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.time.Duration;
-import java.util.logging.ErrorManager;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
 /**
- * Detects if running in GCP and if so format as JSON. If not this will fall back to {@link SimpleFormatter}.
+ * Detects if running in GCP and if so format as JSON. If not this will fall back to {@link SimpleFormatter}. This uses
+ * the existence of the Google Meta-Data Server hostname and switching behaviour accordingly. This can take a few
+ * milliseconds to perform a DNS lookup so you can skip this by setting the environment variable {@code log.isGCP=true}.
  *
  * Example file {@code java.util.logging.config.file} config:
  *
@@ -25,16 +21,13 @@ import java.util.logging.SimpleFormatter;
  * handlers=uk.dansiviter.jule.AsyncConsoleHandler
  *
  * uk.dansiviter.jule.AsyncConsoleHandler.level=FINEST
- * uk.dansiviter.jule.AsyncConsoleHandler.formatter=uk.dansiviter.gcp.log.AutoFormatter
+ * uk.dansiviter.jule.AsyncConsoleHandler.formatter=uk.dansiviter.gcp.log.jul.AutoFormatter
  * # these will be cascaded to JsonFormatter
- * uk.dansiviter.gcp.log.AutoFormatter.decorators=uk.dansiviter.gcp.log.OpenTelemetryTraceDecorator
+ * uk.dansiviter.gcp.log.jul.AutoFormatter.decorators=uk.dansiviter.gcp.log.OpenTelemetryTraceDecorator
  * </pre>
  */
 public class AutoFormatter extends Formatter {
-	private static final URI METADATA_URI = URI.create("http://metadata.google.internal");
-	private static final Duration TIMEOUT = Duration.ofMillis(250);
-
-	private final ErrorManager errorManager = new ErrorManager();
+	private static final String METADATA_HOST = "metadata.google.internal";
 
 	private final Formatter delegate;
 
@@ -54,23 +47,14 @@ public class AutoFormatter extends Formatter {
 	}
 
 	private Boolean detect() {
-		try {
-			var req = HttpRequest
-				.newBuilder(METADATA_URI)
-				.headers("Metadata-Flavor", "Google")
-				.timeout(TIMEOUT)
-				.GET();
-			var client = HttpClient
-				.newBuilder()
-				.connectTimeout(TIMEOUT)
-				.build();
-			client.send(req.build(), discarding());
+		if (Boolean.getBoolean("log.isGcp")) {
 			return true;
-		} catch (IOException e) {
+		}
+		try {
+			InetAddress.getByName(METADATA_HOST);
+			return true;
+		} catch (UnknownHostException e) {
 			// nothing to see here
-		} catch (InterruptedException e) {
-			errorManager.error("Error detecting GCP!", e, ErrorManager.OPEN_FAILURE);
-			currentThread().interrupt();
 		}
 		return false;
 	}
